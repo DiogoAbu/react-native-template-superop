42d3ccb869111e347e57a6215fca4a3ac384c718
DiogoAbu <diogodeazevedosilva@gmail.com>
2021-03-10 14:19:05 -0300

    fix: change props for text component

--- template/src/components/Text.tsx
+++ template/src/components/Text.tsx
@@ -1,13 +1,9 @@
 import React, { FC } from 'react';
-import { StyleProp, Text as NativeText, TextStyle } from 'react-native';
+import { Text as NativeText, TextProps } from 'react-native';
 
 import { useTheme } from '@react-navigation/native';
 
-interface Props {
-  style?: StyleProp<TextStyle>;
-}
-
-const Text: FC<Props> = ({ children, style, ...rest }) => {
+const Text: FC<TextProps> = ({ children, style, ...rest }) => {
   const { colors } = useTheme();
 
   return (

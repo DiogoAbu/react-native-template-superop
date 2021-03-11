d5ecccd037e1050a5ebd1d3bfff642de06deecde
DiogoAbu <diogodeazevedosilva@gmail.com>
2021-03-11 03:54:02 -0300

    fix: update jest setup

--- template/jest.setup.js
+++ template/jest.setup.js
@@ -1,46 +1,13 @@
-jest.mock('react-native-reanimated', () => {
-  const View = require('react-native').View;
+import 'react-native-gesture-handler/jestSetup';
 
-  return {
-    Value: jest.fn(),
-    event: jest.fn(),
-    add: jest.fn(),
-    eq: jest.fn(),
-    set: jest.fn(),
-    cond: jest.fn(),
-    interpolate: jest.fn(),
-    View,
-    Extrapolate: { CLAMP: jest.fn() },
-    Clock: jest.fn(),
-    greaterThan: jest.fn(),
-    lessThan: jest.fn(),
-    startClock: jest.fn(),
-    stopClock: jest.fn(),
-    clockRunning: jest.fn(),
-    not: jest.fn(),
-    or: jest.fn(),
-    and: jest.fn(),
-    spring: jest.fn(),
-    decay: jest.fn(),
-    defined: jest.fn(),
-    call: jest.fn(),
-    Code: View,
-    block: jest.fn(),
-    abs: jest.fn(),
-    greaterOrEq: jest.fn(),
-    lessOrEq: jest.fn(),
-    debug: jest.fn(),
-    Transition: {
-      Together: 'Together',
-      Out: 'Out',
-      In: 'In',
-    },
-    Easing: {
-      in: jest.fn(),
-      out: jest.fn(),
-      inOut: jest.fn(),
-    },
-  };
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+
+  return Reanimated;
 });
 
 jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter.js', () => {
@@ -50,3 +17,4 @@ jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter.js', () => {
 
 // Fix for `useNativeDriver` is not supported because the native animated module is missing.
 jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+
